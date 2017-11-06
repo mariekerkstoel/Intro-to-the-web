464ff541e1e97fd85d75d3d0c36c275113aa89c1
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-06 18:00:19 +0000

    Added post request

--- app.rb
+++ app.rb
@@ -13,10 +13,14 @@ get '/random-cat' do
   erb(:named_cat)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @foo = params[:name]
   @age = params[:age]
   @colour = params[:colour]
   erb(:named_cat)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
