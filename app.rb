00425e02a9646efff35def708c5f0536e176b7e9
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-06 16:20:53 +0000

    Added picture and border

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require 'sinatra'
 
 get '/' do
-  '<h1>HELLO!!!!</h1>'
-end
-
-get '/secret' do
-  'not so secret'
+  '<h1>HELLO!!</h1>'
 end
 
 get '/info' do
-  'ALLAN AND MARIE!'
+  'Allan and Marie'
+end
+
+get '/cat' do
+  '<img style="border:red 5px dotted" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png">'
 end
