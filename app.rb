5e5a6a4d14b0a1bdf558237b8e0c3d3960e28feb
Allan Wazacz <cazwazacz@gmail.com>
2017-11-06 16:29:41 +0000

    Adds views

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ get '/' do
 end
 
 get '/info' do
-  'Allan and Marie'
+  erb(:info)
 end
 
 get '/cat' do
-  '<img style="border:red 5px dotted" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png">'
+  erb(:index)
 end
